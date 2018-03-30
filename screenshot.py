ec72b8e1770145ba5c58524bda593c2a9e967942
Yashwant Nagarjuna <ranbonagarjuna@gmail.com>
2018-03-31 00:48:24 +0530

    Raw test file

--- screenshot.py
+++ screenshot.py
@@ -5,9 +5,7 @@ from mss import mss
 import time
 import os
 
-# image = pyautogui.screenshot()
-# image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-# cv2.imwrite("images/test_image.png", image)
+# Below function taken from Sentedex YouTube Channel
 
 
 def process_image(image):
@@ -15,16 +13,12 @@ def process_image(image):
     processed_image = cv2.Canny(processed_image, threshold1=100, threshold2=300)
     return processed_image
 
+#################################################
 
-# while(True):
-#     screen = np.array(ImageGrab.grab(bbox=(3, 150, 435, 277)))
-#     new_screen = process_image(screen)
-#     print("Loop took {} seconds".format(time.time() - start_time))
-#     start_time = time.time()
-#     cv2.imshow('window', new_screen)
-#     if cv2.waitKey(25) & 0xFF == ord('q'):
-#         cv2.destroyAllWindows()
-#         break
+
+for i in reversed(range(4)):
+    print(i + 1)
+    time.sleep(1)
 
 path = '/Users/Nagarjuna/Projects/ChromeGame/images'
 
@@ -39,11 +33,12 @@ while(True):
     img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
     new_screen = process_image(np.array(img))
     print("Loop took {} seconds".format(time.time() - start_time))
-    start_time = time.time()
     cv2.imshow('test', new_screen)
     # cv2.imwrite('images/frame%d.jpg' % count, new_screen)
     cv2.imwrite(os.path.join(path, 'frame%d.jpg' % count), new_screen)
+    time.sleep(0.3)
     count += 1
+    start_time = time.time()
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
